dcb21a475d46f5727079f31020e4e67a12e1be16
Wayne Brown <wayne.brown@appdynamics.com>
2021-11-17 17:07:43 -0700

    Adding manual instrumentation

--- 3-microservices/services/threads/server.js
+++ 3-microservices/services/threads/server.js
@@ -1,3 +1,22 @@
+// Adding AppD instrumentation
+// Checking to make sure the appdynamics lib is installed.
+try {
+  require.resolve("appdynamics");  
+  console.log("appdynamics resolved");
+
+  require("appdynamics").profile({
+    controllerHostName: 'controller.saas.appdynamics.com',
+    controllerPort: 8090,
+    accountName: 'account',
+    accountAccessKey: 'access-key',
+    applicationName: 'application-name',
+    tierName: 'tier-name',
+    nodeName: 'node-name' // The controller will automatically append the node name with a unique number
+   });   
+} catch (e) {
+  console.error("Could not resolve appdynamics npm");
+}
+
 const app = require('koa')();
 const router = require('koa-router')();
 const db = require('./db.json');
